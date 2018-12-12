4052b74148947f611cea1a3d20e4e1507734b5a0
ren.chalissery@gmail.com <sreee47@gmail.com>
2018-12-12 08:52:49 +0530

    Basic player actions like movement, Vegetable pick up, Dropiing vegtebles to the chop board and extra plate added to the version commit.

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    //Declaring key controllers for player movement
+    public KeyCode moveForward;
+    public KeyCode moveBackward;
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public KeyCode pickItem =KeyCode.F;
+    public KeyCode dropItem = KeyCode.G;
+
+    //Declaring initial speed of the player
+    public float playerSpeed = 1.0f;
+
+    //declaring and initializing inital time for the player 
+    public float timeLeft = 120f;
+
+    //Initial score of the player
+    public int score = 0;
+
+    //List to store the vegetables that the player had picked
+    public List<GameObject> playerBasket;
+
+    //List of vegetables that the player had dropped on to the chop board
+    public List<GameObject> playerChopPlate;
+
+    public List<GameObject> servingPlateItem;
+
+    public GameObject chopBoard;
+    public GameObject servingPlate;
+    public GameObject trashCan;
+
+    private float defaultSpeed;
+    public bool canMove = true;
+	// Use this for initialization
+	void Start () {
+        defaultSpeed = playerSpeed;
+        playerBasket = new List<GameObject>(2);
+        playerChopPlate = new List<GameObject>(3);
+        servingPlateItem = new List<GameObject>(1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer();
+        MovePlayer();
+    }
+
+    //FixedUpdate is called every fixed frames.
+    //This is commonly used for rigidbody movements.
+    private void FixedUpdate()
+    {
+       
+    }
+
+    //Timer function to controle the gametime left and to pause the player once the time is up
+    void Timer()
+    {
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            timeLeft = 0;
+            canMove = false;
+        }
+        
+    }
+
+    //Move the player based on the key inputs.
+    void MovePlayer()
+    {
+     
+        if (canMove)
+        {
+
+            //Move forward
+            if (Input.GetKey(moveForward))
+            {
+      
+                this.gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(0,playerSpeed);
+            }
+
+            //Move backwards
+            else if (Input.GetKey(moveBackward))
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -playerSpeed);
+            }
+
+            //Move Left
+            else if (Input.GetKey(moveLeft))
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-playerSpeed, 0);
+            }
+
+            //Move right
+            else if (Input.GetKey(moveRight)){
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed, 0);
+            }
+
+            // Stay in the same position if any other key is pressed
+            else
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
+
+        }
+        else
+        {
+            return;
+        }
+    }
+    
+    //detects amd sents each frame when the another object is in the trigger collider of the gameobject.
+   void OnTriggerStay2D(Collider2D collision)
+    {
+
+        PickDrop(collision);       
+    }
+
+    //checks wether the player has given the input to pick or drop the vegetables
+    void PickDrop(Collider2D item)
+    {
+
+        if (Input.GetKeyDown(pickItem))
+        {
+            print("f pressed");
+            PickItemFunc(item);
+        }
+        else if (Input.GetKeyDown(dropItem))
+        {
+            print("g pressed");
+            DropItemFunc(item);
+        }
+    }
+
+    //To pick particular vegetable from the rack.
+    //The pick action is implemented based on the collider interaction of the player and the vegetables.
+    void PickItemFunc(Collider2D item)
+    {
+        if(playerBasket.Count < 2)
+        {
+            if(item.gameObject.tag == "VEGETABLES")
+            {
+                print("in pickitem collider");
+                GameObject vegItemPrefab = Instantiate(item.gameObject, transform.position, Quaternion.identity);
+                playerBasket.Add(vegItemPrefab);
+                vegItemPrefab.tag = "Untagged";
+                vegItemPrefab.transform.parent = transform;
+                Vector3 vegPos = vegItemPrefab.transform.position;
+                vegPos.x += playerBasket.Count;
+                vegItemPrefab.transform.position = vegPos;
+
+                
+            }
+
+        }
+
+    }
+
+    //To drop particular vegetable 
+    void DropItemFunc(Collider2D item)
+    {
+        
+      
+        if (playerBasket.Count >0)
+        {
+             // Drop the vegetable to the chopboard
+            if (item.gameObject== chopBoard.gameObject)
+            {
+                
+                if (playerChopPlate.Count<3)
+                {
+                    if (playerBasket.Count > 0)
+                    {
+                        print("in drop logic");
+                        GameObject vegItem = playerBasket[playerBasket.Count - 1];
+                        playerChopPlate.Add(vegItem);
+                        vegItem.transform.parent = chopBoard.gameObject.transform;
+                        playerBasket.Remove(vegItem);
+                        StartCoroutine(ChopingTime());
+                    }
+                }
+            }
+
+            //Dispose to the trashcan.
+            if(item.gameObject.tag == "TrashCan")
+            {
+                
+                Destroy(playerBasket[playerBasket.Count - 1]);
+                playerBasket.Remove(playerBasket[playerBasket.Count-1]);
+                
+                
+            }
+
+            //drop vegetable to the side plates
+            if (item.gameObject == servingPlate.gameObject)
+            {
+                if (servingPlateItem.Count < 1)
+                {
+                    GameObject vegItem = playerBasket[playerBasket.Count - 1];
+                    servingPlateItem.Add(vegItem);
+                    vegItem.transform.parent = servingPlate.gameObject.transform;
+                    playerBasket.Remove(vegItem);
+                }
+                     
+            }
+        }
+    }
+
+    // disable the player movement for chopping vegetables
+     IEnumerator ChopingTime()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(2);
+        canMove = true;
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 Salad-Chef-Programmer-Art is a "Couch Co-Op" simulation game which simulates a small chef art, where the chefs serve the customers with the salad varities they like. The customer has their own likes and dislikes. So the points gained by the chefs depends on the customers happiness.
 
 If the customer is served with a varitey of salad he likes the point gets added for the chef. If the varitey is disliked by the customer then the customer gets angry and the chefs point reduces.If the customer is given a proper salad within brfore 70% of waiting time then customer happiness level increases and then the chef is prized with a random powerup like speed boost, extra points and extra time. The game ends when the timmer goes zero and the chef with the maximum points wins the game.
+
+#UPDATE
+1. Basic level structure for the game is added. The assets used for the design is placed in the 2d       Assets folder.
+
+2. Basic player actions added for second player. The player has the functionalities for:-
+    * Moving in all four directions.
+    * Picking up a vegetable of his/her choice.
+    * Dropping the chosen vegetables to the extra plate  and the choppping board.
+    * Combination of salad can be made by droping different varites of veggies to the chopping board.
+
+3. Player action is disabled for a point of time if the player keeps a vegetable on the chopping  board.
+
+
+#Upcomming Updates
+1. Picking Combination of salad from the chopping board. 
+2. picking up veggies placed over the extra plate.
+3. Customer Management.
+4. Score Management for the player.
+5. Timer management.
+6. Winner Decesion.
+
